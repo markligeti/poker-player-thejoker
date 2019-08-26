dbbe48e5e485083c6c960cdcc4f51ec44000f607
markligeti <ligetimark.e@gmail.com>
2019-08-26 11:36:43 +0200

    Revert "Error fixed"
    This reverts commit 9e810942

--- player.py
+++ player.py
@@ -1,34 +1,29 @@
+from random import randint
+
+
 class Player:
-    VERSION = "1.5.4"
+    VERSION = "1.3"
 
     def bet_request(self, game_state):
-        # small_blind = game_state['small_blind']
-        #
-        # highest_bet = self.get_player_bets(game_state)
-        #
-        # if highest_bet > small_blind * 2:
-        #     return highest_bet + small_blind
-        #
-        # else:
-        # print(f"kutyámajom!")
-        return game_state["current_buy_in"] - game_state["players"]["in_action"]["bet"]
+        small_blind = game_state['small_blind']
+
+        highest_bet = self.get_player_bets(game_state)
+
+        if highest_bet > small_blind * 2:
+            return highest_bet + small_blind
+
+        else:
+            return small_blind * 2
 
     def showdown(self, game_state):
         pass
 
-    # def get_hole_cards(self, game_state):
-    #     for player in game_state["players"]:
-    #         if player["name"] == "TheJoker":
-    #             return player["hole_cards"]
-    #
-    # def get_com_cards(self, game_state):
-    #     return game_state["community_cards"]
-    #
-    # def get_player_bets(self, game_state):
-    #     player_bet = 0
-    #
-    #     for player in game_state['players']:
-    #         if player_bet < player['bet']:
-    #             player_bet = player['bet']
-    #
-    #     return player_bet
+    def get_player_bets(self, game_state):
+
+        player_bet = 0
+
+        for player in game_state['players']:
+            if player_bet < player_bet['bet']:
+                player_bet = player['bet']
+
+        return player_bet

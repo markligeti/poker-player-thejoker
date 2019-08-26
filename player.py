584adfd69a0a65d187a386aa8e6c55e99d0060d2
adamzopcsak <adamzopcsak@gmail.com>
2019-08-26 10:49:57 +0200

    version update and code formatting

--- player.py
+++ player.py
@@ -2,22 +2,21 @@ from random import randint
 
 
 class Player:
-    VERSION = "1.1"
+    VERSION = "1.2"
 
-    def betRequest(self, game_state):
+    def bet_request(self, game_state):
         small_blind = game_state['small_blind']
 
         highest_bet = self.get_player_bets(game_state)
 
-        if highest_bet > small_blind*2:
-            return highest_bet + randint(1,10)
+        if highest_bet > small_blind * 2:
+            return highest_bet + randint(1, 10)
 
         else:
-            return small_blind*2
+            return small_blind * 2
 
     def showdown(self, game_state):
         pass
-
 
     def get_player_bets(self, game_state):
 
@@ -25,8 +24,6 @@ class Player:
 
         for player in game_state['player']:
             if player_bet < player_bet['bet']:
-             player_bet = player['bet']
-
+                player_bet = player['bet']
 
         return player_bet
-

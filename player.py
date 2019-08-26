5c96d425deacf0962f84552ee150dcd6ad1e44ee
markligeti <ligetimark.e@gmail.com>
2019-08-26 11:56:37 +0200

    1.8.2 all in v5

--- player.py
+++ player.py
@@ -1,30 +1,18 @@
-from random import randint
-
-
 class Player:
-    VERSION = "1.8"
+    VERSION = "1.8.2"
 
     def bet_request(self, game_state):
-        # small_blind = game_state['small_blind']
-        #
-        # highest_bet = self.get_player_bets(game_state)
-        #
-        # if highest_bet > small_blind * 2:
-        #     return highest_bet + small_blind
-        #
-        # else:
-
         for player in game_state["players"]:
-            if player["name"] == "TheJoker":
+            if player["id"] == game_state["in_action"]:
+                print(player["stack"])
                 return int(player["stack"])
 
-        # return game_state["players"][game_state["in_action"]]["stack"]
+        # return int(game_state["players"][game_state["in_action"]]["stack"])
 
     def showdown(self, game_state):
         pass
 
     def get_player_bets(self, game_state):
-
         player_bet = 0
 
         for player in game_state['players']:
@@ -32,3 +20,6 @@ class Player:
                 player_bet = player['bet']
 
         return player_bet
+
+
+

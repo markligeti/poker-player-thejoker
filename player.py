629d077ffb082f2e490fca24b22ce7c7ab8f411d
Gergely Koltai <kgalmos@gmail.com>
2019-08-26 10:27:25 +0200

    now betting properly

--- player.py
+++ player.py
@@ -3,7 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        small_blind = game_state['small_blind']
+        return small_blind * 2
 
     def showdown(self, game_state):
         pass

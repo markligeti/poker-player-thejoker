8128bd6cfc8f4162e4107e0cb3c7747a63279f56
markligeti <ligetimark.e@gmail.com>
2019-08-26 16:04:06 +0200

    v 4.0.5 check flush

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 class Player:
-    VERSION = "3.0"
+    VERSION = "4.0.5"
 
     players = None
     my_id = None
     round = None
     hand = None
     com_cards = None
+    all_cards = None
 
     def betRequest(self, game_state):
         self.players = game_state["players"]
@@ -13,6 +14,7 @@ class Player:
         self.round = game_state["round"]
         self.com_cards = game_state["community_cards"]
         self.hand = self.get_hand()
+        self.all_cards = self.hand + self.com_cards
 
         bet = self.set_own_bet()
         return bet
@@ -31,6 +33,8 @@ class Player:
             return 0
         elif self.check_for_pairs():
             return self.get_my_stack()
+        elif self.check_flush():
+            return self.get_my_stack()
         else:
             return 0
 
@@ -46,3 +50,24 @@ class Player:
         for player in self.players:
             if player["id"] == self.my_id:
                 return player["hole_cards"]
+
+    def check_flush(self):
+        diamonds = []
+        hearts = []
+        spades = []
+        clubs = []
+
+        for card in self.all_cards:
+            if card["suit"] == "diamonds":
+                diamonds.append(card)
+            elif card["suit"] == "hearts":
+                hearts.append(card)
+            elif card["suit"] == "spades":
+                spades.append(card)
+            elif card["suit"] == "clubs":
+                clubs.append(card)
+
+        if len(diamonds) >= 4 or len(hearts) >= 4 or len(spades) >= 4 or len(clubs) >= 4:
+            return True
+        else:
+            return False

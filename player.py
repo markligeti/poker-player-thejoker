3ee7ae387174038540d94c5efb5f7838fc64b11b
Gergely Koltai <kgalmos@gmail.com>
2019-08-26 10:47:56 +0200

    Added highest player bet calculation

--- player.py
+++ player.py
@@ -1,11 +1,32 @@
+from random import randint
+
 
 class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
         small_blind = game_state['small_blind']
-        return small_blind * 2
+
+        highest_bet = self.get_player_bets(game_state)
+
+        if highest_bet > small_blind*2:
+            return highest_bet + randint(1,10)
+
+        else:
+            return small_blind*2
 
     def showdown(self, game_state):
         pass
+
+
+    def get_player_bets(self, game_state):
+
+        player_bet = 0
+
+        for player in game_state['player']:
+            if player_bet < player_bet['bet']:
+             player_bet = player['bet']
+
+
+        return player_bet
 
